06f78dc3d05bdfb1f48bec10083b86b17d2c0bdc
Erik M. Buck <erik.buck@sbcglobal.net>
2021-02-23 19:19:16 -0500

    Produced sample of design documentation as a starter for ongoing design with respect to Requirements

---
+++ WSUWord.hpp
@@ -0,0 +1,27 @@
+//
+//  WSUWord.hpp
+//  
+//
+//  Created by Erik M. Buck on 2/23/21.
+//
+
+#ifndef WSUWord_hpp
+#define WSUWord_hpp
+
+#include <string>
+
+///
+/// \defgroup Model Model Subsystem
+/// @{
+///  The subsystems of the application, WSUWord, are organized according the Model-View-Controller design. This Module contains components that are part of the Model subsystem.
+
+/// Ref: Req. 1.0
+class WSUWordModel {
+   /// Ref: Req. 1.0
+   String m_string;
+};
+
+
+/**@}*/
+
+#endif /* WSUWord_hpp */
